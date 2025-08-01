d64516ad5592a130df32617ae7329c82b1b498f1
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-01 11:27:08 +0530

    Add code

--- src/main/java/com/JournalApp/JournalApp/Controller/JournalController.java
+++ src/main/java/com/JournalApp/JournalApp/Controller/JournalController.java
@@ -2,8 +2,9 @@ package com.JournalApp.JournalApp.Controller;
 
 import com.JournalApp.JournalApp.Service.JournalService;
 import com.JournalApp.JournalApp.model.Journal;
-import com.JournalApp.JournalApp.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/journal")
 public class JournalController {
     @Autowired
-    private JournalService journalService;
-//    @GetMapping
-//    public void createJournal(@RequestBody Journal journal){
-//        journalService.addJournalEntry(journal);
-//    }
+    protected JournalService journalService;
+    @GetMapping
+    public ResponseEntity<Journal> allJournal(@RequestBody Journal journal){
+        journalService.getAllJournal(journal);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/JournalApp/JournalApp/Service/JournalService.java
+++ src/main/java/com/JournalApp/JournalApp/Service/JournalService.java
@@ -5,14 +5,18 @@ import com.JournalApp.JournalApp.model.Journal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class JournalService {
     @Autowired
     private JournalRepository journalRepository;
-    public void addJournalEntry(Journal journal){
-        journalRepository.save(journal);
-    }
-    public void saveJournal(Journal journal){
 
+    public List<Journal> getAllJournal(Journal journal){
+        return journalRepository.findAll();
+    }
+
+    public void saveJournal(Journal journal){
+        journalRepository.save(journal);
     }
 }
